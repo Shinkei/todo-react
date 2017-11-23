27901407463650e9fdf904b712b6a1de9d5c597e
Shinkei <shinkeygentatsu@yahoo.com.mx>
2017-11-22 21:20:15 -0500

    appli the bindActionCreatos patters from redux to bind the actions from the reducer with the actions from the elements

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {bindActionCreators} from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import {updateCurrentAction} from './reducers/todo';
 
-// we start handling the actions only in the reducer and import them to use in another places
-const todoChangeHandler = value => store.dispatch(updateCurrentAction(value));
-
+// Apply a pattern from redux to bind the action from the rducer with an action of the element
+const actions = bindActionCreators({ todoChangeHandler: updateCurrentAction}, store.dispatch);
+//-----------------------------------element action-----reducer action--------dispatch
 const render = () => {
     // get a global state from the store for the todo objects
     const state = store.getState();
     
     // send the state as props to the App component
     ReactDOM.render(
-        <App todos={state.todos} currentTodo={state.currentTodo} changeCurrent={todoChangeHandler}/>,
+        <App todos={state.todos} currentTodo={state.currentTodo} changeCurrent={actions.todoChangeHandler}/>,
         document.getElementById('root'));
 }
 
