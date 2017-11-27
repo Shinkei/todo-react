9b885226119ebc3621ae198947a331d4835c8e25
Shinkei <shinkeygentatsu@yahoo.com.mx>
2017-11-26 20:56:46 -0500

    add some comments about the devtools extencion for redux

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension'; // devtools extension so we can use the browser's add-on
 import reducer from './reducers/todo';
 
 // create the store from redux ans add the reducer to handle the actions
 export default createStore(
     reducer,
     composeWithDevTools(applyMiddleware(thunk))// Thunk help us to do asyncronous actions
-);
+);                                             // the compose let us check the store in the browser
